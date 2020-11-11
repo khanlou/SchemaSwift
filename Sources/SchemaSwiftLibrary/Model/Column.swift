badc031cec935e8d74b6d586c37f7d70371b93e1
Soroush Khanlou <soroushkhanlou@gmail.com>
2020-11-10 21:43:05 -0500

    flesh out remainder of postgres types

--- Sources/SchemaSwiftLibrary/Model/Column.swift
+++ Sources/SchemaSwiftLibrary/Model/Column.swift
@@ -17,23 +17,67 @@ public struct Column {
     public let name: String
     public let udtName: String
     public let isNullable: Bool
-
-
-
+    
+    // https://www.postgresql.org/docs/9.5/datatype.html
+    // https://github.com/SweetIQ/schemats/blob/master/src/schemaPostgres.ts#L17-L93
     public var swiftType: String {
         switch udtName {
+        case "bpchar":
+        case "char":
+        case "varchar":
         case "text":
+        case "citext":
+        case "bytea":
+        case "inet":
+        case "time":
+        case "timetz":
+        case "interval":
+        case "name":
             return "String"
-        case "int4":
-            return "Int"
         case "uuid":
             return "UUID"
-        case "timestamptz":
-            return "Date"
+        case "int2":
+        case "int4":
+        case "int8":
+            return "Int"
+        case "float4":
+        case "float8":
+            return "Double"
         case "bool":
             return "Bool"
+        case "date":
+        case "timestamp":
+        case "timestamptz":
+            return "Date"
+        case "_int2":
+        case "_int4":
+        case "_int8":
+            return "[Int]"
+        case "_float4":
+        case "_float8":
+            return "[Double]"
+        case "_bool":
+            return "[Bool]"
+        case "_varchar":
+        case "_text":
+        case "_citext":
+        case "_bytea":
+            return "[String]"
+        case "_uuid":
+            return "[UUID]"
+        case "_timestamptz":
+            return "[Date]"
+        case "numeric":
+        case "money":
+        case "_numeric":
+        case "_money":
+        case "oid":
+        case "json":
+        case "jsonb":
+        case "_json":
+        case "_jsonb":
         default:
-            return "Unknown! \\ postgres type: \(udtName)"
+            return "Any! \\Unknown postgres type: \(udtName)"
         }
     }
 }
